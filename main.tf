e1de8f0525ed2a80244f771fa15913890e59a889
tgremlin <allantenison@live.com>
2020-06-09 14:57:54 -0500

    Moving to flat file
    Will get all objects created and working, and then refactor into modules

--- main.tf
+++ main.tf
@@ -4,3 +4,32 @@ features {
     
 }
 }
+
+resource "azurerm_resource_group" "rg" {
+    name     = "NextCloud"
+    location = "eastus"
+tags = {
+    name = "NextCloud"
+}
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                      = "nextcloudstorage"
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  
+  tags = {
+      environment = "Devlopment"
+  }
+}
+
+resource "azurerm_virtual_network" "private_network" {
+  name              = "NC-Private"
+  address_space     = ["10.0.0.0/16"]
+  location          = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+}
+
+
